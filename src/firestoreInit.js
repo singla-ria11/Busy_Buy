1caddd7e71fa8942e13bd2aa1b4d7b3e43953ece
Ria Singla <singla.ria11@gmail.com>
2025-03-07 02:01:40 +0530

    Secure the firebase configuration

--- src/firestoreInit.js
+++ src/firestoreInit.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPDqLDRxmoR8aj-VRcvKnudnh1Gwnp228",
-  authDomain: "busy-buy-a184d.firebaseapp.com",
-  projectId: "busy-buy-a184d",
-  storageBucket: "busy-buy-a184d.firebasestorage.app",
-  messagingSenderId: "436015685838",
-  appId: "1:436015685838:web:d62a83f74ccf75d170f09f",
-  measurementId: "G-97NB9H254J",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
